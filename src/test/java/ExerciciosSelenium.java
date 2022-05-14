75f0969dd2aa65126274bd0fb678dbfba5270c41
Nogueira <michele.nogueira@capgemini.com>
2022-05-14 15:21:00 -0300

    Exercicio03

--- src/test/java/ExerciciosSelenium.java
+++ src/test/java/ExerciciosSelenium.java
@@ -1,3 +1,5 @@
+import org.junit.After;
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -10,6 +12,7 @@ public class ExerciciosSelenium {
 
     @Test
     public void testeExercicosSelenium() {
+        //Exercicio 01
         driver.get("file:///C:/Users/minoguei/OneDrive%20-%20Capgemini/Documents/Register.html");
         driver.findElement(By.xpath("//body[@class='ng-scope']/section[@id='section']/div[@class='container center']/div[contains(@class,'row')]/div[@class='col-sm-6 col-md-6 col-xs-12']/form[@id='basicBootstrapForm']/div[1]/div[1]/input[1]")).sendKeys("Flavio");
         driver.findElement(By.xpath("//body[@class='ng-scope']/section[@id='section']/div[@class='container center']/div[contains(@class,'row')]/div[@class='col-sm-6 col-md-6 col-xs-12']/form[@id='basicBootstrapForm']/div[@class='form-group']/div[2]/input[1]"))
@@ -17,5 +20,23 @@ public class ExerciciosSelenium {
         driver.findElement(By.xpath("//textarea[@ng-model='Adress']")).sendKeys("AV Paulista, 509");
         driver.findElement(By.xpath("//input[@type='email']")).sendKeys("teste@teste.com.br");
         driver.findElement(By.xpath(" //input[@type='tel']")).sendKeys("119773512");
+
+        //Exercicio 02
+        driver.findElement(By.xpath("//*[@value='Male']")).click();
+        Assert.assertTrue(driver.findElement(By.xpath("//*[@value='Male']")).isSelected());
+
+        //Exercicio03
+        driver.findElement(By.xpath("//input[@id='checkbox1']")).click();
+        Assert.assertTrue("Checkbox não selecionado", driver.findElement(By.xpath("//input[@id='checkbox1']")).isSelected());
+        Assert.assertFalse("Checkbox selecionado", driver.findElement(By.xpath("//label[contains(text(),'Movies')]")).isSelected());
+
     }
+
+    @After
+    public void after() throws InterruptedException {
+        Thread.sleep(5000);
+        driver.quit();
+    }
+
 }
+
