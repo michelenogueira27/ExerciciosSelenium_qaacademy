553ea2343f869357f73603969853679f8e21178a
Nogueira <michele.nogueira@capgemini.com>
2022-05-14 16:17:13 -0300

    Exercicio04completo

--- src/test/java/ExerciciosSelenium.java
+++ src/test/java/ExerciciosSelenium.java
@@ -3,7 +3,9 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class ExerciciosSelenium {
 
@@ -11,7 +13,7 @@ public class ExerciciosSelenium {
 
 
     @Test
-    public void testeExercicosSelenium() {
+    public void testeExercicosSelenium() throws InterruptedException {
         //Exercicio 01
         driver.get("file:///C:/Users/minoguei/OneDrive%20-%20Capgemini/Documents/Register.html");
         driver.findElement(By.xpath("//body[@class='ng-scope']/section[@id='section']/div[@class='container center']/div[contains(@class,'row')]/div[@class='col-sm-6 col-md-6 col-xs-12']/form[@id='basicBootstrapForm']/div[1]/div[1]/input[1]")).sendKeys("Flavio");
@@ -30,6 +32,40 @@ public class ExerciciosSelenium {
         Assert.assertTrue("Checkbox não selecionado", driver.findElement(By.xpath("//input[@id='checkbox1']")).isSelected());
         Assert.assertFalse("Checkbox selecionado", driver.findElement(By.xpath("//label[contains(text(),'Movies')]")).isSelected());
 
+        //Exercicio 04
+
+        driver.findElement(By.xpath("//div[@id='msdd']")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//a[contains(text(),'Portuguese')]")).click();
+
+
+        //Listas Option
+
+        //com Click, tem que navegar até o elemento option e extrair o xpath
+        driver.findElement(By.xpath("//option[@value='Java']")).click();
+        Thread.sleep(2000);
+
+        //Selecionar Lista Option/Select com Select Selenium
+        WebElement selectElement = driver.findElement(By.xpath("//select[@id='Skills']"));
+        Select select = new Select(selectElement);
+        select.selectByVisibleText("Linux");
+
+        //Countries
+        driver.findElement(By.xpath("//span[@class='select2-selection select2-selection--single']")).click();
+        driver.findElement(By.xpath("//*[@class='select2-results__options']//*[contains(text(),'United States of America')]")).click();
+
+        // Birthday
+
+        driver.findElement(By.xpath("//option[@value='1988']")).click();
+        driver.findElement(By.xpath("//option[@value='April']")).click();
+        driver.findElement(By.xpath("//option[@value='15']")).click();
+
+        //Password
+
+        driver.findElement(By.xpath("//input[@id='firstpassword']")).sendKeys("teste123");
+        driver.findElement(By.xpath("//input[@id='secondpassword']")).sendKeys("teste1223");
+        driver.findElement(By.xpath("//button[@id='submitbtn']")).click();
+
     }
 
     @After
@@ -37,6 +73,7 @@ public class ExerciciosSelenium {
         Thread.sleep(5000);
         driver.quit();
     }
+
 
 }
 
